1e70f63fc25db90ccc46e10eb08f9d63f533cddf
Alex Velasquez <jtinnesco@gmail.com>
2019-12-25 20:43:06 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -5,14 +5,3 @@ class CreateStudents < ActiveRecord::Migration[5.1]
     end
   end
 end
-
-
-
-def change
-  create_table :artists do |t|
-    t.string :name
-    t.string :genre
-    t.integer :age
-    t.string :hometown
-  end
-end
